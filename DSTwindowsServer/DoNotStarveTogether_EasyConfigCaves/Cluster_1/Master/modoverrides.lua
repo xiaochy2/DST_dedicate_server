c1a6ecb9e454d69fc44a2f9f1e8a18e91ed8de9d
Tim Yang <xiaochy2@uci.edu>
2025-03-05 00:05:45 -0600

    Adding config file for window server

---
+++ DSTwindowsServer/DoNotStarveTogether_EasyConfigCaves/Cluster_1/Master/modoverrides.lua
@@ -0,0 +1,42 @@
+return {
+  ["workshop-1530801499"]={
+    configuration_options={
+      ArrowsignEnable=false,
+      CountdownEnable=false,
+      HungerCost=1,
+      Ownership=true,
+      SanityCost=1 
+    },
+    enabled=true 
+  },
+  ["workshop-347079953"]={
+    configuration_options={ DFV_Language="EN", DFV_MinimalMode="default" },
+    enabled=true 
+  },
+  ["workshop-362175979"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
+  ["workshop-375850593"]={ configuration_options={  }, enabled=true },
+  ["workshop-375859599"]={
+    configuration_options={
+      divider=5,
+      random_health_value=0,
+      random_range=0,
+      send_unknwon_prefabs=false,
+      show_type=0,
+      unknwon_prefabs=1,
+      use_blacklist=true 
+    },
+    enabled=true 
+  },
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  } 
+}
---
+++ DSTwindowsServer/DoNotStarveTogether_EasyConfigCaves/Cluster_1/dedicated_server_mods_setup.lua
@@ -0,0 +1,12 @@
+-- Display Food Values
+ServerModSetup("347079953")
+-- Wormhole Marks
+ServerModSetup("362175979")
+-- Extra Equip Slots
+ServerModSetup("375850593")
+-- Global Positions
+ServerModSetup("378160973")
+-- 木牌传送 (GUI)
+ServerModSetup("2119742489")
+-- Health Info
+ServerModSetup("375859599")
